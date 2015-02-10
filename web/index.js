ca875c874fecc1aed2522cc29485707937e5b3aa
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2015-02-10 12:55:37 -0500

    added sync with hash. hopefully made lit<->ctor un/escaping work for real

--- web/index.js
+++ web/index.js
@@ -3,46 +3,85 @@
     
     var Controls = React.createClass({
         getInitialState: function() {
-            return {
+            var parts = this.parseHash() || {
                 pattern: '',
-                flags: [],
-                tree: undefined
+                flags: ''
+            }
+            parts.tree = this.textsToTree(parts)
+            return parts
+        },
+        componentDidMount: function() {
+            window.addEventListener('hashchange', this.updateSelfFromHash)
+        },
+
+        readHash: function() {
+            return decodeURIComponent(window.location.hash.slice(1))
+        },
+        parseHash: function() {
+            // TODO test
+            var hash = this.readHash()
+            var commaIndex = hash.indexOf(',')
+            if (commaIndex <= 0) { return }
+            var patternLen = Number(hash.slice(0, commaIndex))
+            if (isNaN(patternLen)) { return }
+            var flagsIndex = commaIndex + 1 + patternLen
+            return {
+                pattern: hash.slice(commaIndex + 1, flagsIndex),
+                flags: hash.slice(flagsIndex)
             }
         },
-        textsToViz: function(texts) {
-            var tree
+        updateSelfFromHash: function() {
+            var parts = this.parseHash()
+            if (parts) {
+                this.handleTextsChange(parts)
+            }
+        },
+        updateHash: function(parts) {
+            var hash = parts.pattern.length
+                + ','
+                + parts.pattern
+                + parts.flags
+            if (hash === this.readHash()) {
+                return
+            }
+            window.location.hash = encodeURIComponent(hash)
+        },
+
+        textsToTree: function(parts) {
             try {
-                tree = parser.parse(texts.pattern)
+                return parser.parse(parts.pattern)
             } catch(e) {
                 // don't propagate b/c still want text states updated
-                // TODO highlight text or something
+                // TODO highlight text location
                 console.error('parsing failed', e)
             }
-            this.setState({
-                pattern: texts.pattern,
-                flags: texts.flags,
-                tree: tree
-            })
+            
         },
-        vizToTexts: function(someData) {
-            // TODO treeToPattern
-            this.setState({
-                // pattern: treeToPattern(someData.tree),
-                flags: someData.flags,
-                tree: someData.tree
-            })
+        handleTextsChange: function(parts) {
+            if ( this.state.pattern === parts.pattern
+                && this.state.flags === parts.flags ) {
+                return
+            }
+            var state = {
+                pattern: parts.pattern,
+                flags: parts.flags,
+                tree: this.textsToTree(parts)
+            }
+            this.updateHash(state)
+            this.setState(state)
         },
+
         render: function() {
             return (
                 <div className="controls">
                     <reactClasses.Texts
                         pattern={this.state.pattern}
                         flags={this.state.flags}
-                        onChange={this.textsToViz} />
+                        onChange={this.handleTextsChange} />
                     <reactClasses.Surface
                         tree={this.state.tree}
-                        flags={this.state.flags}
-                        onChange={this.vizToTexts} />
+                        flags={this.state.flags} />
+                    
                 </div>
             )
         }
